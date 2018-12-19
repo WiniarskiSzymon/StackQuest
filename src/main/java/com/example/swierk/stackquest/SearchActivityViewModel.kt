c9f34c6da875376e51e6cd2d726e9a80a5ba4170
WiniarskiSzymon <57-43-57-6&4gh>
2018-12-19 22:23:53 +0100

    First LiveData version

--- src/main/java/com/example/swierk/stackquest/SearchActivityViewModel.kt
+++ src/main/java/com/example/swierk/stackquest/SearchActivityViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.swierk.stackquest
 
+import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.example.swierk.stackquest.api.StackAPI
@@ -12,25 +13,25 @@ class SearchActivityViewModel @Inject constructor(private val stackAPI: StackAPI
 
 
     private lateinit var disposable: Disposable
-    val loadingBarVisibility: MutableLiveData<Int> = MutableLiveData()
-    val errorMessage:MutableLiveData<Int> = MutableLiveData()
 
 
+    private lateinit var searchResponse: MutableLiveData<SearchResponse>
+
+    fun searchQuery(query : String): LiveData<SearchResponse> {
+        if (!::searchResponse.isInitialized) {
+            searchResponse = MutableLiveData()
+            getQueryResults(query)
+        }
+        return searchResponse
+    }
 
     private fun getQueryResults(query : String){
         disposable = stackAPI.getQueryResults(query)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .doOnSubscribe { onQueruSearchingStart() }
-            .doOnSuccess { onQuerySearchingFinished(it) }
+            .doOnSuccess { searchResponse.value = it }
             .subscribe()
     }
-
-    private fun onQueruSearchingStart(searchResponse: SearchResponse){
-        loadingBarVisibility.value = View.VISIBLE
-        errorMessage.value = null
-    }
-
 
 
     override fun onCleared() {
